99fee1b120d290537e56e022f62e764844a40ba8
Jasper Hoffmann <hoffmaja@informatik.uni-freiburg.de>
2026-07-05 11:38:41 +0000

    Update imports for leap-c core-only restructure (#338), add moviepy test dep
    leap-c #338 moved broadcast_default_param/ParamSplits/n_segments from
    leap_c.utils.parameters to leap_c.parameters.utils. Update the four
    planners and the cartpole acados_ocp accordingly. Add moviepy to dev
    deps so gymnasium.utils.save_video imports in test_cartpole.

--- leapc_lab/mass_spring_damper/planner.py
+++ leapc_lab/mass_spring_damper/planner.py
@@ -6,7 +6,7 @@ import torch
 from leap_c.diff_mpc.function import AcadosDiffMpcCtx
 from leap_c.torch import AcadosDiffMpcTorch
 from leap_c.utils.collate import collate_acados_diff_mpc_ctx
-from leap_c.utils.parameters import broadcast_default_param
+from leap_c.parameters.utils import broadcast_default_param
 
 from leapc_lab.mass_spring_damper.acados_ocp import (
     export_parametric_ocp,
